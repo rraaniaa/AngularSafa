aa3a10fb30b561ae2d56bfabe717f754c3cddc9a
rraaniaa <raniachebbi87@example.com>
2024-02-05 15:23:32 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DiagnosticComponent } from './diagnostic/diagnostic.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path: 'diagnostic', component: DiagnosticComponent },
+    { path: '', component: HomeComponent },
+    
+];
---
+++ src/app/diagnostic/diagnostic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagnostic',
+  standalone: true,
+  imports: [],
+  templateUrl: './diagnostic.component.html',
+  styleUrl: './diagnostic.component.css'
+})
+export class DiagnosticComponent {
+
+}
